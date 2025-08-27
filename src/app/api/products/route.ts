0e8e1a40afed6ac2fa88d2631bf1a5b5b8c347c6
ludvikroman7 <ludvikroman7@gmail.com>
2025-08-27 10:39:17 +0200

    Create route.ts

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,10 @@
+// src/app/api/products/route.ts
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  const products = await prisma.product.findMany({
+    orderBy: { name: "asc" },
+    take: 1000,
+  });
+  return Response.json(products);
+}
