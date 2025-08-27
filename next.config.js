2510e413aaed3f42e403ef970b84d346aaf225b5
ludvikroman7 <ludvikroman7@gmail.com>
2025-08-27 08:22:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // nic experimental – App Router je v Next 13/14 defaultně
 };
 
 module.exports = nextConfig;
