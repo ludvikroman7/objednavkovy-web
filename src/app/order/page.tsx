f98d013c38f5016d7498b7ed51a27341edbe137c
ludvikroman7 <ludvikroman7@gmail.com>
2025-08-27 09:50:10 +0200

    Create page.tsx

---
+++ src/app/order/page.tsx
@@ -0,0 +1,8 @@
+export default function OrderPage() {
+  return (
+    <main style={{ padding: "24px", fontFamily: "sans-serif" }}>
+      <h1>Objednávky</h1>
+      <p>Tady bude objednávkový formulář. (Dočasná stránka)</p>
+    </main>
+  );
+}
